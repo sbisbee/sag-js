6a9292b8cbadf6680bf216206e516afa0c216a72
Sam Bisbee <sam@sbisbee.com>
2011-12-15 17:32:17 -0500

    a few quick tests for get() and put()

--- tests/browser/sag.js
+++ tests/browser/sag.js
@@ -68,9 +68,71 @@ asyncTest('setDatabase() and create if does not exist', function() {
       equal(resp._HTTP.status, 404, 'expect it to not be there');
 
       couch.setDatabase(dbName, true, function(exists) {
-        ok(exists, 'db should exist by now');
+        ok(exists, 'db should exist now');
 
         start();
+      });
+    }
+  });
+});
+
+asyncTest('get()', function() {
+  var couch;
+  expect(2);
+
+  couch = makeCouch(true);
+
+  couch.get({
+    url: '',
+    callback: function(resp) {
+      equal(resp._HTTP.status, 200, 'got a 200');
+      equal(typeof resp.body, 'object', 'body is decoded');
+
+      start();
+    }
+  });
+});
+
+asyncTest('put()', function() {
+  var couch;
+  var docID = 'wellhellothere';
+  var docData = {
+    _id: docID,
+    how: 'you doin',
+    worked: true
+  };
+
+  expect(4);
+
+  couch = makeCouch(true);
+
+  couch.put({
+    id: docID,
+    data: docData,
+    callback: function(resp) {
+      equal(resp._HTTP.status, 201, 'got a 201');
+      equal(resp.body.id, docID, 'got the id');
+
+      docData._rev = resp.body.rev;
+
+      couch.put({
+        id: docID,
+        data: docData,
+        callback: function(resp) {
+          equal(resp._HTTP.status, 201, 'got a 201');
+
+          //for the next call and its deepEqual()
+          docData._rev = resp.body.rev;
+
+          couch.get({
+            url: docID,
+            callback: function(resp) {
+              deepEqual(resp.body, docData, 'got the body');
+
+              start();
+            }
+          });
+        }
       });
     }
   });
