7d0564159e5467e45e23609cad25441a49c612d2
Sam Bisbee <sam@sbisbee.com>
2012-04-06 16:53:37 -0400

    Fixing serverFromURL() for Node v0.4.x (different parseURL results).

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@ exports.serverFromURL = function(url) {
   var parts;
   var sagRes;
 
+  var i;
+
   if(!url || typeof url !== 'string') {
     throw TypeError('You must provide a complete URL as a string.');
   }
@@ -9,7 +11,15 @@ exports.serverFromURL = function(url) {
   parts = parseURL(url);
 
   //create the server
+  i = parts.host.indexOf('@');
+
+  if(i > -1) {
+    parts.host = parts.host.split('@')[1];
+  }
+
+  i = null;
   parts.host = parts.host.split(':');
+
   sagRes = exports.server(parts.host.shift(), parts.host.shift());
 
   //log the user in (if provided)
@@ -25,6 +35,9 @@ exports.serverFromURL = function(url) {
   //set the database (if there's a path)
   if(typeof parts.path === 'string' && parts.path.length > 1) {
     sagRes.setDatabase(parts.path.split('/')[1]);
+  }
+  else if(typeof parts.pathname === 'string' && parts.pathname.length > 1) {
+    sagRes.setDatabase(parts.pathname.split('/')[1]);
   }
 
   return sagRes;
