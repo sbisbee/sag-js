ccba1be42d5b5468c7f70e7983a31911562e6c7e
Sam Bisbee <sam@sbisbee.com>
2013-03-04 17:24:47 -0500

    Don't require login callback for makeCouch.

--- tests/node/makeCouch.js
+++ tests/node/makeCouch.js
@@ -1,14 +1,17 @@
 function makeCouch(setDB, loginCallback) {
   var couch = sag.server('localhost', '5984');
 
-  if(loginCallback) {
-    couch.login({
-      user: 'admin',
-      pass: 'passwd',
-      type: sag.AUTH_COOKIE,
-      callback: loginCallback
-    });
+  var loginOpts = {
+    user: 'admin',
+    pass: 'passwd',
+    type: sag.AUTH_COOKIE
+  };
+
+  if(typeof loginCallback === 'function') {
+    loginOpts.callback = loginCallback;
   }
+
+  couch.login(loginOpts);
 
   if(setDB) {
     couch.setDatabase(dbName);
