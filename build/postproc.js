719ed66170a895c60cbe289fc0eca8e689fd2586
Sam Bisbee <sam@sbisbee.com>
2013-06-19 23:29:14 -0400

    Need uglify for minification

--- build/postproc.js
+++ build/postproc.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 var src = fs.readFileSync(process.argv[2], 'utf8');
 
+if(!src) {
+  throw new Error('No content in source file');
+}
+
 src = '/* Sag-JS v' + process.argv[3] + ' saggingcouch.com\n'
       + 'Licensed under Apache 2.0 */\n' + src;
 
